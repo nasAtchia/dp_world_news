354517b9dcd59c329390d57f5a3eec7316507cd9
Annas Atchia <nasatchia@gmail.com>
2023-04-11 00:52:37 +0400

    Add World News Block

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,9 @@ All notable changes to this module will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.0.0] - 2023-04-11
+
+### Added
+- Add hook_preprocess_HOOK() to register variables for the World News Block.
+- Add World News block with dummy data to display latest world news.
---
+++ src/Plugin/Block/WorldNewsBlock.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\dp_world_news\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Latest world news' block.
+ *
+ * @Block(
+ *   id = "dp_world_news",
+ *   admin_label = @Translation("World News"),
+ *   category = @Translation("World News")
+ * )
+ */
+class WorldNewsBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    $build['articles'] = [
+      '#markup' => [
+        [
+          'content' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+          'description' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa',
+          'image_url' => 'https://dummyimage.com/600x400/000/fff',
+          'published_at' => '2023-01-01T00:00:00Z',
+          'source' => 'Example.com',
+          'title' => 'Article 1',
+          'url' => 'https://example.com/',
+        ],
+        [
+          'content' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+          'description' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa',
+          'image_url' => 'https://dummyimage.com/600x400/000/fff',
+          'published_at' => '2023-01-01T00:00:00Z',
+          'source' => 'Example.com',
+          'title' => 'Article 2',
+          'url' => 'https://example.com/',
+        ],
+      ],
+    ];
+
+    return $build;
+  }
+
+}
