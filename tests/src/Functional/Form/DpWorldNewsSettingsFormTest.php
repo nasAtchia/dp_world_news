2ad283966655abeb47116e984e995611cc4a7701
Annas Atchia <nasatchia@gmail.com>
2023-04-14 21:26:53 +0400

    Add functional test for settings form

---
+++ tests/src/Functional/Form/DpWorldNewsSettingsFormTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\Tests\dp_world_news\Functional\Form;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the DpWorldNewsSettingsForm class.
+ *
+ * @group dp_world_news
+ */
+class DpWorldNewsSettingsFormTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'dp_world_news',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * A simple user.
+   *
+   * @var \Drupal\user\Entity\User
+   */
+  private $user;
+
+  /**
+   * Perform initial setup tasks that run before every test method.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function setUp(): void {
+    parent::setUp();
+    $this->user = $this->drupalCreateUser([
+      'administer site configuration',
+    ]);
+  }
+
+  /**
+   * Tests the form.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   * @throws \Behat\Mink\Exception\ResponseTextException
+   */
+  public function testSettingsForm(): void {
+    // Login.
+    $this->drupalLogin($this->user);
+
+    // Access the config page.
+    $this->drupalGet('/admin/config/content/world-news');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Test the form elements exist and have defaults.
+    $this->assertSession()->fieldValueEquals(
+      'news_api_api_key',
+      '',
+    );
+    $this->assertSession()->fieldValueEquals(
+      'news_data_api_key',
+      '',
+    );
+
+    // Test form submission.
+    $this->submitForm(
+      [
+        'news_api_api_key' => 'api_key',
+        'news_data_api_key' => 'api_key',
+      ],
+      t('Save configuration')
+    );
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('The configuration options have been saved.');
+
+    // Test the new values are there.
+    $this->drupalGet('/admin/config/content/world-news');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->fieldValueEquals(
+      'news_api_api_key',
+      'api_key',
+    );
+    $this->assertSession()->fieldValueEquals(
+      'news_data_api_key',
+      'api_key',
+    );
+  }
+
+}
